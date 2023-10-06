5440dad66bf67f41f4bc70b9780b5418b17e887e
spongzi <spongzicode@163.com>
2023-10-06 19:28:18 +0800

    使用lombok

--- spongzi-club-subject/spongzi-club-infra/src/main/java/com/spongzi/subject/infra/basic/entity/SubjectCategory.java
+++ spongzi-club-subject/spongzi-club-infra/src/main/java/com/spongzi/subject/infra/basic/entity/SubjectCategory.java
@@ -1,125 +1,64 @@
 package com.spongzi.subject.infra.basic.entity;
 
+import lombok.Data;
+
 import java.util.Date;
 import java.io.Serializable;
 
 /**
  * 题目分类(SubjectCategory)实体类
  *
- * @author makejava
+ * @author spongzi
  * @since 2023-10-06 16:57:54
  */
+@Data
 public class SubjectCategory implements Serializable {
+
     private static final long serialVersionUID = 412015292565140186L;
+
     /**
      * 主键
      */
     private Long id;
+
     /**
      * 分类名称
      */
     private String categoryName;
+
     /**
      * 分类类型
      */
     private Integer categoryType;
+
     /**
      * 图标连接
      */
     private String imageUrl;
+
     /**
      * 父级id
      */
     private Long parentId;
+
     /**
      * 创建人
      */
     private String createdBy;
+
     /**
      * 创建时间
      */
     private Date createdTime;
+
     /**
      * 更新人
      */
     private String updateBy;
+
     /**
      * 更新时间
      */
     private Date updateTime;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Integer getCategoryType() {
-        return categoryType;
-    }
-
-    public void setCategoryType(Integer categoryType) {
-        this.categoryType = categoryType;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public Long getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(Long parentId) {
-        this.parentId = parentId;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public Date getCreatedTime() {
-        return createdTime;
-    }
-
-    public void setCreatedTime(Date createdTime) {
-        this.createdTime = createdTime;
-    }
-
-    public String getUpdateBy() {
-        return updateBy;
-    }
-
-    public void setUpdateBy(String updateBy) {
-        this.updateBy = updateBy;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
 }
 
