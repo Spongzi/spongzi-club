a7c5dce89dbb6fffa08ddccbdbe6ede0317dd993
spongzi <spongzicode@163.com>
2023-10-06 20:09:50 +0800

    添加统一返回结果类

---
+++ spongzi-club-subject/spongzi-club-common/src/main/java/com/spongzi/subject/common/enums/ResultCodeEnum.java
@@ -0,0 +1,36 @@
+package com.spongzi.subject.common.enums;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 返回结果代码
+ *
+ * @author spong
+ * @date 2023/10/06
+ */
+@Getter
+public enum ResultCodeEnum {
+
+    SUCCESS(200, "成功"),
+    FAIL(500, "失败");
+
+    private final int code;
+
+    private final String desc;
+
+    ResultCodeEnum(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public static ResultCodeEnum getByCode(int codeVal) {
+        for (ResultCodeEnum resultCodeEnum : ResultCodeEnum.values()) {
+            if (resultCodeEnum.code == codeVal) {
+                return resultCodeEnum;
+            }
+        }
+        return null;
+    }
+}
