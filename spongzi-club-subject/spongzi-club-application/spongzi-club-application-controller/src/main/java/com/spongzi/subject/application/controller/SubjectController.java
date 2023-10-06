83cf86deeb237ed0bbc62a45ed68c0da6b1e7c85
spongzi <spongzicode@163.com>
2023-10-06 17:22:04 +0800

    mysql, druid, mybatis plus整合，以及对配置文件的修改和简单的测试

--- spongzi-club-subject/spongzi-club-application/spongzi-club-application-controller/src/main/java/com/spongzi/subject/application/controller/SubjectController.java
+++ spongzi-club-subject/spongzi-club-application/spongzi-club-application-controller/src/main/java/com/spongzi/subject/application/controller/SubjectController.java
@@ -1,7 +1,11 @@
 package com.spongzi.subject.application.controller;
 
+import com.spongzi.subject.infra.basic.entity.SubjectCategory;
+import com.spongzi.subject.infra.basic.service.SubjectCategoryService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * 刷题controller
@@ -12,8 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SubjectController {
 
+    @Resource
+    private SubjectCategoryService subjectCategoryService;
+
     @GetMapping("/test")
     public String test() {
-        return "hello club";
+        SubjectCategory subjectCategory = subjectCategoryService.queryById(1L);
+        return subjectCategory.getCategoryName();
     }
 }
--- spongzi-club-subject/spongzi-club-start/src/main/java/com/spongzi/subject/SubjectApplication.java
+++ spongzi-club-subject/spongzi-club-start/src/main/java/com/spongzi/subject/SubjectApplication.java
@@ -1,5 +1,6 @@
 package com.spongzi.subject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan("com.spongzi")
+@MapperScan("com.spongzi.**.mapper")
 public class SubjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(SubjectApplication.class, args);
