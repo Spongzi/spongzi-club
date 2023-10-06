b8c2bcdd208c743decaf4dc799ccaedc66cc2dea
spongzi <spongzicode@163.com>
2023-10-06 20:37:03 +0800

    引入guava,使用preconditions来进行参数校验

--- spongzi-club-subject/spongzi-club-application/spongzi-club-application-controller/src/main/java/com/spongzi/subject/application/controller/SubjectCategoryController.java
+++ spongzi-club-subject/spongzi-club-application/spongzi-club-application-controller/src/main/java/com/spongzi/subject/application/controller/SubjectCategoryController.java
@@ -1,12 +1,14 @@
 package com.spongzi.subject.application.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.google.common.base.Preconditions;
 import com.spongzi.subject.application.convert.SubjectCategoryConvert;
 import com.spongzi.subject.application.dto.SubjectCategoryDTO;
 import com.spongzi.subject.common.entity.Result;
 import com.spongzi.subject.domain.entity.SubjectCategoryBO;
 import com.spongzi.subject.domain.service.SubjectCategoryDomainService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,18 +31,23 @@ public class SubjectCategoryController {
     private SubjectCategoryDomainService subjectCategoryDomainService;
 
     @PostMapping("/add")
-    public Result<Boolean> add(@RequestBody SubjectCategoryDTO subjectCategoryDTO) {
+    public Result<String> add(@RequestBody SubjectCategoryDTO subjectCategoryDTO) {
         try {
             if (log.isInfoEnabled()) {
                 log.info("SubjectCategoryController.add.dto: {}", JSON.toJSONString(subjectCategoryDTO));
             }
+
+            Preconditions.checkNotNull(subjectCategoryDTO.getCategoryType(), "分类类型不能为空");
+            Preconditions.checkArgument(StringUtils.isEmpty(subjectCategoryDTO.getCategoryName()), "分类的名称不能为空");
+            Preconditions.checkNotNull(subjectCategoryDTO.getParentId(), "分类父级id不能为空");
+
             SubjectCategoryBO subjectCategoryBO = SubjectCategoryConvert.INSTANCE
                     .convertBoToCategory(subjectCategoryDTO);
             subjectCategoryDomainService.add(subjectCategoryBO);
             return Result.ok();
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return Result.fail();
+            log.error("SubjectCategoryController.add.error: {}", e.getMessage(), e);
+            return Result.fail(e.getMessage());
         }
     }
 
