18348ea0d58198c52bcc5fe39eaa7012986ba480
spongzi <spongzicode@163.com>
2023-10-07 20:02:34 +0800

    开发标签模块，生成简单的代码

---
+++ spongzi-club-subject/spongzi-club-infra/src/main/java/com/spongzi/subject/infra/basic/service/SubjectLabelService.java
@@ -0,0 +1,45 @@
+package com.spongzi.subject.infra.basic.service;
+
+import com.spongzi.subject.infra.basic.entity.SubjectLabel;
+
+/**
+ * 题目标签表(SubjectLabel)表服务接口
+ *
+ * @author makejava
+ * @since 2023-10-07 19:39:32
+ */
+public interface SubjectLabelService {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    SubjectLabel queryById(Long id);
+
+    /**
+     * 新增数据
+     *
+     * @param subjectLabel 实例对象
+     * @return 实例对象
+     */
+    SubjectLabel insert(SubjectLabel subjectLabel);
+
+    /**
+     * 修改数据
+     *
+     * @param subjectLabel 实例对象
+     * @return 实例对象
+     */
+    SubjectLabel update(SubjectLabel subjectLabel);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    boolean deleteById(Long id);
+
+}
---
+++ spongzi-club-subject/spongzi-club-infra/src/main/java/com/spongzi/subject/infra/basic/service/impl/SubjectLabelServiceImpl.java
@@ -0,0 +1,69 @@
+package com.spongzi.subject.infra.basic.service.impl;
+
+import com.spongzi.subject.infra.basic.entity.SubjectLabel;
+import com.spongzi.subject.infra.basic.mapper.SubjectLabelDao;
+import com.spongzi.subject.infra.basic.service.SubjectLabelService;
+import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
+import javax.annotation.Resource;
+
+/**
+ * 题目标签表(SubjectLabel)表服务实现类
+ *
+ * @author makejava
+ * @since 2023-10-07 19:39:32
+ */
+@Service("subjectLabelService")
+public class SubjectLabelServiceImpl implements SubjectLabelService {
+    @Resource
+    private SubjectLabelDao subjectLabelDao;
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    @Override
+    public SubjectLabel queryById(Long id) {
+        return this.subjectLabelDao.queryById(id);
+    }
+
+    /**
+     * 新增数据
+     *
+     * @param subjectLabel 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public SubjectLabel insert(SubjectLabel subjectLabel) {
+        this.subjectLabelDao.insert(subjectLabel);
+        return subjectLabel;
+    }
+
+    /**
+     * 修改数据
+     *
+     * @param subjectLabel 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public SubjectLabel update(SubjectLabel subjectLabel) {
+        this.subjectLabelDao.update(subjectLabel);
+        return this.queryById(subjectLabel.getId());
+    }
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    @Override
+    public boolean deleteById(Long id) {
+        return this.subjectLabelDao.deleteById(id) > 0;
+    }
+}
