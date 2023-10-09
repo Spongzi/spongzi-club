ff63db1cb6caf3ee7fece1c438dd83a4fe460ead
spongzi <spongzicode@163.com>
2023-10-09 14:38:50 +0800

    封装分页

---
+++ spongzi-club-subject/spongzi-club-common/src/main/java/com/spongzi/subject/common/entity/PageInfo.java
@@ -0,0 +1,30 @@
+package com.spongzi.subject.common.entity;
+
+import lombok.Data;
+
+/**
+ * 分页信息
+ *
+ * @author spong
+ * @date 2023/10/09
+ */
+public class PageInfo {
+
+    private Integer pageNo = 1;
+
+    private Integer pageSize = 20;
+
+    public Integer getPageNo() {
+        if (pageNo == null || pageNo < 1) {
+            return 1;
+        }
+        return pageNo;
+    }
+
+    public Integer getPageSize() {
+        if (pageSize == null || pageSize < 1) {
+            return 10;
+        }
+        return pageSize;
+    }
+}
