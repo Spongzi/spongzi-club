a982e58de874a2bd73f6e5242ead3f16b6610d23
spongzi <spongzicode@163.com>
2023-10-31 16:13:46 +0800

    实现缓存读写权限

--- spongzi-club-gateway/src/main/java/com/spongzi/club/gateway/auth/StpInterfaceImpl.java
+++ spongzi-club-gateway/src/main/java/com/spongzi/club/gateway/auth/StpInterfaceImpl.java
@@ -1,9 +1,15 @@
 package com.spongzi.club.gateway.auth;
 
 import cn.dev33.satoken.stp.StpInterface;
+import com.alibaba.cloud.commons.lang.StringUtils;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.spongzi.club.gateway.redis.RedisUtil;
 import org.springframework.stereotype.Component;
 
-import java.util.LinkedList;
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,23 +21,42 @@ import java.util.List;
 @Component
 public class StpInterfaceImpl implements StpInterface {
 
+    @Resource
+    private RedisUtil redisUtil;
+
+    private final String AUTH_PERMISSION_PREFIX = "auth.permission";
+
+    private final String AUTH_ROLE_PREFIX = "auth.role";
+
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
         // 返回此 loginId 拥有的权限列表
         // 1. 直接跟数据库交互
         // 2. 可以去redis缓存
         // 3. 先从缓存中去拿，没有的话可以去调用微服务去拿
-        List<String> permissionList = new LinkedList<>();
-        permissionList.add("user:add");
-        return permissionList;
+        return getStrings(AUTH_PERMISSION_PREFIX, loginId);
     }
 
     @Override
     public List<String> getRoleList(Object loginId, String loginType) {
         // 返回此 loginId 拥有的角色列表
-        List<String> roleList = new LinkedList<>();
-        roleList.add("admin");
-        return roleList;
+        return getStrings(AUTH_ROLE_PREFIX, loginId);
+    }
+
+    /**
+     * 根据身份得到角色权限列表
+     *
+     * @param authRolePrefix 身份验证角色前缀
+     * @param loginId        登录ID
+     * @return {@link List}<{@link String}>
+     */
+    private List<String> getStrings(String authRolePrefix, Object loginId) {
+        String authKey = redisUtil.buildKey(authRolePrefix, loginId.toString());
+        String authValue = redisUtil.get(authKey);
+        if (StringUtils.isNotBlank(authValue)) {
+            return Collections.emptyList();
+        }
+        return new Gson().fromJson(authValue, new TypeToken<ArrayList<String>>(){}.getType());
     }
 
 }
