6fd5db53f3b906a91d06fde6124b79191c8ecfe4
spongzi <spongzicode@163.com>
2023-10-31 09:59:38 +0800

    简单的集成sa-token测试demo

---
+++ spongzi-club-auth/spongzi-club-auth-application/spongzi-club-auth-application-controller/src/main/java/com/spongzi/auth/application/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.spongzi.auth.application.controller;
+
+import cn.dev33.satoken.stp.SaTokenInfo;
+import cn.dev33.satoken.stp.StpUtil;
+import cn.dev33.satoken.util.SaResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/")
+public class UserController {
+
+    // 测试登录，浏览器访问： http://localhost:3011/user/doLogin?username=zhang&password=123456
+    @RequestMapping("doLogin")
+    public SaResult doLogin(String username, String password) {
+        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
+        if ("zhang".equals(username) && "123456".equals(password)) {
+            StpUtil.login(10001);
+            // 第2步，获取 Token  相关参数
+            SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
+            // 第3步，返回给前端
+            return SaResult.data(tokenInfo);
+        }
+        return SaResult.error("登录失败");
+    }
+
+    // 查询登录状态，浏览器访问： http://localhost:3011/user/isLogin
+    @RequestMapping("isLogin")
+    public String isLogin() {
+        return "当前会话是否登录：" + StpUtil.isLogin();
+    }
+
+}
