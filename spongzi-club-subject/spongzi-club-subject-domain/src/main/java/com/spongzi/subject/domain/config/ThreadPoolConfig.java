5fb6032a0abd0afb098f316c681db304cc9e958d
spongzi <spongzicode@163.com>
2023-12-03 16:39:26 +0800

    feat：自定义名称线程工厂

--- spongzi-club-subject/spongzi-club-subject-domain/src/main/java/com/spongzi/subject/domain/config/ThreadPoolConfig.java
+++ spongzi-club-subject/spongzi-club-subject-domain/src/main/java/com/spongzi/subject/domain/config/ThreadPoolConfig.java
@@ -3,7 +3,6 @@ package com.spongzi.subject.domain.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -18,13 +17,13 @@ import java.util.concurrent.TimeUnit;
 public class ThreadPoolConfig {
 
     @Bean(name = "labelThreadPool")
-    public ThreadPoolExecutor getLabelThreadPool(){
+    public ThreadPoolExecutor getLabelThreadPool() {
         return new ThreadPoolExecutor(20,
                 100,
                 5,
                 TimeUnit.SECONDS,
                 new LinkedBlockingDeque<>(40),
-                Executors.defaultThreadFactory(),
+                new CustomNameThreadFactory("label"),
                 new ThreadPoolExecutor.CallerRunsPolicy());
     }
 
